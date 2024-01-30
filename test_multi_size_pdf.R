e7b2349f9e9291f4b479d9103a5c7cecbcdca46c
Christopher Bottoms, PhD <Christopher-Bottoms@omrf.org>
2024-01-30 21:22:10 +0000

    Simplify test code

--- test_multi_size_pdf.R
+++ test_multi_size_pdf.R
@@ -6,27 +6,21 @@ source("multi_size_pdf.R")
 
 plot_list <- list()
 
+df <- data.frame(x = c(1:3), y = c(4:6))
+
 plot_list[["a"]] <- list(
                          # ggplot object
-                         ggplot(data.frame(x=c(1:3), y=c(4:6)),
-                           aes(x = x, y = y)) +
-                         geom_point(),
-
+                         ggplot(df, aes(x = x, y = y)) + geom_point(),
                          4, #height
-
                          4  # width
                     )
 
 plot_list[["b"]] <- list(
-                         ggplot(data.frame(x=c(1:3), y=c(4:6)),
-                            aes(x = x, y = y)) +
-                         geom_point(),
+                         ggplot(df, aes(x = x, y = y)) + geom_point(),
                          4 # no width given
                     )
-plot_list[["c"]] <- list(
-                         ggplot(data.frame(x=c(1:3), y=c(4:6)),
-                           aes(x = x, y = y)) +
-                         geom_point() # no height/width given
-                    )
+
+# no height/width given
+plot_list[["c"]] <- list(ggplot(df, aes(x = x, y = y)) + geom_point())
 
 multi_size_pdf(plot_list, "dummy_out_file_combined.pdf")
